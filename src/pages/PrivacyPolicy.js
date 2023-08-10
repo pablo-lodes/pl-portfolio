13ea6d27c3767dc9d99994b3804d8ba25f28af89
pablo-lodes <pablo.lodes@gmail.com>
2023-08-10 14:36:21 +0200

    Created Privacy Policy

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className="privacy-policy">
+            <h2>Privacy Policy</h2>
+            <div className="privacy-policy-content">
+                <p className="privacy-section">Privacy Statement</p>
+                <p>
+                    Your privacy is important to us. It is our policy to respect your privacy regarding any
+                    information we may collect from you across our website.
+                </p>
+
+                <p className="privacy-section">Information We Collect</p>
+                <p>We only collect information that is relevant to your use of our website.</p>
+
+                <p className="privacy-section">How We Use Your Information</p>
+                <p>We use the information we collect in various ways, including:</p>
+                <ul>
+                    <li>...</li>
+                    <li>...</li>
+                </ul>
+
+                <p className="privacy-section">Disclosure of Your Information</p>
+                <p>We may disclose your personal information to third parties:</p>
+                <ul>
+                    <li>...</li>
+                    <li>...</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
